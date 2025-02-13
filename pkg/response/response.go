1c1f26745081f901a6ede23c05365674765e581e
saniyar-dev <saniyar.dev@gmail.com>
2025-02-13 16:29:13 +0330

    [ ADD ] Response object

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -1,1 +1,25 @@
 package response
+
+import (
+	"net/http"
+
+	"github.com/grafana/sobek"
+	"github.com/saniyar-dev/xk6-new-http/pkg/interfaces"
+	"go.k6.io/k6/js/modules"
+)
+
+// Response object
+type Response struct {
+	http.Response
+
+	Vu modules.VU
+
+	Obj *sobek.Object
+}
+
+var _ interfaces.Object = &Response{}
+
+// Define func defines data properties on obj attatched to Client struct.
+func (c *Response) Define() error {
+	return nil
+}
