282760cd742b5c106aeb3db8b5f8e063f1bf2bb4
saniyar-dev <saniyar.dev@gmail.com>
2025-02-16 21:22:52 +0330

    [ ADD ] basics of event handling for client and its general interface

---
+++ pkg/client/event.go
@@ -0,0 +1,54 @@
+package client
+
+import (
+	"fmt"
+
+	"github.com/saniyar-dev/xk6-new-http/pkg/events"
+	"github.com/saniyar-dev/xk6-new-http/pkg/helpers"
+	"github.com/saniyar-dev/xk6-new-http/pkg/interfaces"
+)
+
+const (
+	OPEN     = "open"
+	CLOSE    = "close"
+	ERROR    = "error"
+	RESPONSE = "responseReceived"
+	REQUEST  = "requestToBeSent"
+)
+
+type eventListeners struct {
+	open     *events.EventListener
+	close    *events.EventListener
+	error    *events.EventListener
+	response *events.EventListener
+	request  *events.EventListener
+}
+
+var _ interfaces.EventListeners = &eventListeners{}
+
+func (es *eventListeners) GetListener(t string) (*events.EventListener, error) {
+	switch t {
+	case OPEN:
+		return es.open, nil
+	case CLOSE:
+		return es.close, nil
+	case ERROR:
+		return es.error, nil
+	case RESPONSE:
+		return es.response, nil
+	case REQUEST:
+		return es.request, nil
+	default:
+		return nil, fmt.Errorf("unsupported event type for client %s", t)
+	}
+}
+
+func (es *eventListeners) New() interfaces.EventListeners {
+	return &eventListeners{
+		open:     helpers.NewEventListener(OPEN),
+		close:    helpers.NewEventListener(CLOSE),
+		error:    helpers.NewEventListener(ERROR),
+		response: helpers.NewEventListener(RESPONSE),
+		request:  helpers.NewEventListener(REQUEST),
+	}
+}
--- pkg/interfaces/interfaces.go
+++ pkg/interfaces/interfaces.go
@@ -1,6 +1,9 @@
 package interfaces
 
-import "github.com/grafana/sobek"
+import (
+	"github.com/grafana/sobek"
+	"github.com/saniyar-dev/xk6-new-http/pkg/events"
+)
 
 // Params interface defines the common behavior/functionalities each object params exported on the main API should have.
 // Client, Request, TCP, etc. are objects and params you pass to them while initializing them are object params.
@@ -12,4 +15,9 @@ type Object interface {
 	sobek.DynamicObject
 	Define() error
 	ParseParams(*sobek.Runtime, []sobek.Value) (Params, error)
+}
+
+type EventListeners interface {
+	GetListener(string) (*events.EventListener, error)
+	New() EventListeners
 }
