9566d3d26c5241f63f7bf1feaa5d71188e348abf
saniyar-dev <saniyar.dev@gmail.com>
2025-02-16 18:28:41 +0330

    [ ADD ] EventListener struct to handle list of event listeners

--- pkg/events/events.go
+++ pkg/events/events.go
@@ -1,1 +1,23 @@
 package events
+
+import "github.com/grafana/sobek"
+
+type EventListener struct {
+	EventType string
+
+	List []func(sobek.Value) (sobek.Value, error)
+}
+
+func (e *EventListener) NewListener(evType string) *EventListener {
+	return &EventListener{
+		EventType: evType,
+	}
+}
+
+func (e *EventListener) Add(fn func(sobek.Value) (sobek.Value, error)) {
+	e.List = append(e.List, fn)
+}
+
+func (e *EventListener) All() []func(sobek.Value) (sobek.Value, error) {
+	return e.List
+}
